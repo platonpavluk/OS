c37ce812bcf85dda043a04f1603f390397c2f25d
ADMIN <platon12802014@gmail.com>
2024-11-22 19:54:46 +0200

    TWO

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-mod vga_buffer; // Подключаем модуль с буфером VGA
-mod command; // Подключаем модуль с обработкой команд
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -11,20 +10,10 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// Заглушка для имитации чтения ввода
-fn read_command() -> &'static str {
-    "hello" // Для примера, всегда возвращаем "hello"
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something(); // Выводим подсказку
-    
-    // Читаем команду
-    let command = read_command();
-    
-    // Обрабатываем команду через модуль commands
-    command::process_command(command);
+    vga_buffer::print_something(); // Выводим подсказ
     
     loop {}
 }
